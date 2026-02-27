d91eead0a28700f15eaf95d984f7a1b870c66c30
Josh Fisher <joshuafisher1110@gmail.com>
2026-02-27 14:39:04 -0500

    All VR Tour files

---
+++ app-files/data.js
@@ -0,0 +1,364 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-sidewalk",
+      "name": "Sidewalk",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.4337299731675692
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2909669001496873,
+          "pitch": 0.14739389853533957,
+          "rotation": 0,
+          "target": "1-front-porch"
+        },
+        {
+          "yaw": 0.7278917093343935,
+          "pitch": 0.030230307175834525,
+          "rotation": 0,
+          "target": "2-volleyball-court"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.3294500500003892,
+          "pitch": -0.3792160526337227,
+          "title": "Phi Kappa Theta",
+          "text": "The house of the Phi Kappa Theta fraternity at Trine University built in 2007."
+        },
+        {
+          "yaw": 2.5859980576690482,
+          "pitch": -0.016991055308062997,
+          "title": "Trine University",
+          "text": "The Trine campus is where the brothers of Phi Kappa Theta take classes and participate in student activities."
+        }
+      ]
+    },
+    {
+      "id": "1-front-porch",
+      "name": "Front Porch",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0666385251962307,
+          "pitch": 0.31770164038664284,
+          "rotation": 0,
+          "target": "0-sidewalk"
+        },
+        {
+          "yaw": 1.3732318192725916,
+          "pitch": 0.09692327098394138,
+          "rotation": 0,
+          "target": "2-volleyball-court"
+        },
+        {
+          "yaw": -0.03378850540147482,
+          "pitch": 0.34986013724844334,
+          "rotation": 0,
+          "target": "3-foyer"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.344926269450214,
+          "pitch": 0.040520255959924256,
+          "title": "Alpha Sigma Phi",
+          "text": "The brothers of Phi Kappa Theta and Alpha Sigma Phi are close friends and are always able to count on each other."
+        },
+        {
+          "yaw": 2.801114020156052,
+          "pitch": -0.2786303667458654,
+          "title": "American Flag",
+          "text": "This flag is raised at sunrise and lowered and folded at sunset every day. Respect for the American Flag is a very important part of our brotherhood."
+        }
+      ]
+    },
+    {
+      "id": "2-volleyball-court",
+      "name": "Volleyball Court",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.06536128084480985,
+          "pitch": -0.0043370302150282924,
+          "title": "Theta Phi Alpha",
+          "text": "The house of the Trine's Theta Alpha Phi sorority, and old house of Phi Kappa Theta"
+        }
+      ]
+    },
+    {
+      "id": "3-foyer",
+      "name": "Foyer",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.0909467132245485,
+          "pitch": 0.2864216884931903,
+          "rotation": 0,
+          "target": "4-l-whitney-hanzel-chapter-room"
+        },
+        {
+          "yaw": -3.122710899670931,
+          "pitch": 0.5558626513549587,
+          "rotation": 0,
+          "target": "1-front-porch"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.9768291432175324,
+          "pitch": 0.34941456971681717,
+          "title": "Basement",
+          "text": "<div>The basement is where brothers go to hang out, listen to music, and host social functions with other Greek life organizations.</div>"
+        },
+        {
+          "yaw": 1.7492777010287757,
+          "pitch": 0.08535899575916162,
+          "title": "2025 - 2026 Composite",
+          "text": "This composite pictures all of the active brothers during the 2025-2026 school year."
+        }
+      ]
+    },
+    {
+      "id": "4-l-whitney-hanzel-chapter-room",
+      "name": "L. Whitney Hanzel Chapter Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9786243084626625,
+          "pitch": 0.2705991965616459,
+          "rotation": 0,
+          "target": "5-apt-3"
+        },
+        {
+          "yaw": -1.6393066569110832,
+          "pitch": 0.2519586379654175,
+          "rotation": 0,
+          "target": "3-foyer"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6530629542537714,
+          "pitch": 0.322542510218252,
+          "title": "Founder's Cups",
+          "text": "The Phi Kappa Theta chapter at Trine has won 5 Founder's Cups including 4 consecutive trophies. The award is given to the best Phi Kap chapter in the nation based on PR Hours, Social events, Fundraising totals, etc."
+        },
+        {
+          "yaw": -2.807463566974379,
+          "pitch": 0.4570611093166175,
+          "title": "John F. Kennedy",
+          "text": "<div data-subtree=\"aimfl,mfl\" style=\"display: contents\" data-processed=\"true\">John F. Kennedy was made an honorary member of the </div><span data-processed=\"true\">Theta Kappa Phi&nbsp;</span>(now <span data-wiz-uids=\"wdzQvc_c\" data-processed=\"true\">Phi Kappa Theta)</span>&nbsp;at Worcester Polytechnic Institute in Worcester, Massachusetts."
+        }
+      ]
+    },
+    {
+      "id": "5-apt-3",
+      "name": "Apt. 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -1.456518602837404,
+        "pitch": 0.13941232101448797,
+        "fov": 1.4337299731675692
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2074198862737031,
+          "pitch": 0.47572982948382503,
+          "rotation": 0,
+          "target": "4-l-whitney-hanzel-chapter-room"
+        },
+        {
+          "yaw": 1.0024308059076894,
+          "pitch": 0.8842860975324864,
+          "rotation": 0,
+          "target": "3-foyer"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.7364972829221337,
+          "pitch": 0.19946569972152695,
+          "title": "President's and Treasurer's rooms",
+          "text": "These rooms are given to the acting President and Treasurer elected each year."
+        },
+        {
+          "yaw": 1.5751878824730614,
+          "pitch": 0.22422367274598187,
+          "title": "Phi Kappa Theta Badge",
+          "text": "The Phi Kappa Theta badge's elements correlate to several foundations of Phi Kappa Theta's pillars and beliefs."
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
